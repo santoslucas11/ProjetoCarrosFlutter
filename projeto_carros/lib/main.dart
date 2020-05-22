58b5e88aa5f75da7c746ae04453c9a620de0f523
santoslucas11 <santoslucas.dev@gmail.com>
2020-05-22 12:46:44 -0300

    primeiro commit

--- projeto_carros/lib/main.dart
+++ projeto_carros/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue[100],
       ),
       home: LoginPage(),
     );
