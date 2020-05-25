0356ecd6f5fc598d213d49665b51aa051a3e3da9
santoslucas11 <santoslucas.dev@gmail.com>
2020-05-25 18:54:45 -0300

    Parser Json simples

--- projeto_carros/lib/pages/login_api.dart
+++ projeto_carros/lib/pages/login_api.dart
@@ -23,6 +23,14 @@ class LoginApi{
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
+    Map mapResponse = json.decode(response.body);
+
+    String nome       = mapResponse["nome"];
+    String email      = mapResponse ["e-mail"];
+
+    print("Nome: $nome");
+    print("Email: $email");
+
   return true;
   }
 }
